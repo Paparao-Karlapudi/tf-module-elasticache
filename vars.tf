b6e9627b48a445ee28b415976203efb7ac316f7e
pappik <pappik@gmail.com>
2023-11-15 23:23:03 -0500

    documentdb

--- vars.tf
+++ vars.tf
@@ -2,6 +2,6 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
-variable "num_node_groups" {}
+variable "num_cache_nodes" {}
 variable "node_type" {}
-variable "replicas_per_node_group" {}
+variable "engine_version" {}
